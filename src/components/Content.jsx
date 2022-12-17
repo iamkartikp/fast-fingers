b45f27d4dc158627b835bdc56300e57f57e51efc
Kartik <kartik.ipatil10@gmail.com>
2022-12-17 22:18:40 +0530

    Feat: Added text to speech

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,25 +1,68 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
+import ModalComponent from './common/Modal';
+import useSpeechSynthesis from 'react-speech-kit/dist/useSpeechSynthesis';
 
 export default function Content() {
     const [words] = useState("Something wasn't right The farmer's market buzzed around him, his arms heavy with his cloth totes filled to the brim. There'd been a lovely selection of candles this morning. Now he waited among the sea of humanity in the square for the curly-haired vendor to wrap his fresh-cut flowers. An acoustic band played in the middle of the market, twangy notes bouncing off tents and muddled with the voices of the crowd. He reached out for his wrapped bouquet with a smile while alarm bells rang in his head. It seemed like a pleasant day- from all the available information, it was. Yet every time he turned a corner, every time he bumped into someone and mumbled his apologies, he felt as if eyes were burning into the back of his skull. He checked his breath rate and found it functionally normal. He tested his blinking mod and found no concerns. Still, as he weaved through the market, he zoned in on the port-o-johns and stood in line as calmly as he could.");
     // const [word] = useState("");
     let [index, setIndex] = useState(0);
     let [score, setScore] = useState(0);
-    let [seconds] = useState(60);
-    let [accuracy] = useState(0);
+    let [seconds, setSeconds] = useState(60);
+    let [isTimerStart, setTimerStart] = useState(false);
+    let [isModalOpen, setModal] = useState(false);
+    const { speak } = useSpeechSynthesis();
 
     const handleEnteredWords = (e) => {
         let word1 = e.target.value
-        if(e.key === " ") {
+        if (word1.length) {
+            if (!isTimerStart) {
+                startTimer();
+            }
+        }
+        if (e.key === " ") {
+            speak({ text: words.split(" ")[index+1] })
             // setEnteredWords(word1.trim())
-            if(word1.trim().split(" ")[word1.trim().split(" ").length-1] === words.split(" ")[index]) {
-                setScore(score+1);
+            if (word1.trim().split(" ")[word1.trim().split(" ").length - 1] === words.split(" ")[index]) {
+                setScore(score + 1);
                 document.querySelector(`.class${index}`).classList.add('active');
             } else {
                 document.querySelector(`.class${index}`).classList.add('inactive');
             }
-            setIndex(index+1)
+            setIndex(index + 1)
         }
+    }
+
+    useEffect(() => {
+        if (isTimerStart) {
+            const interval = setInterval(() => {
+                setSeconds(seconds - 1);
+            }, 1000);
+
+            if (seconds <= 0) {
+                calculateFinalScore();
+            }
+
+            return () => {
+                clearInterval(interval);
+            };
+        }
+    });
+
+    const startTimer = () => {
+        setTimerStart(true);
+    }
+
+    const calculateFinalScore = () => {
+        setModal(true);
+        setTimerStart(false);
+        setSeconds(60);
+        setIndex(0);
+        setScore(0);
+    }
+
+    const setModalClose = () => {
+        setModal(false)
     }
 
     return (
@@ -32,9 +75,9 @@ export default function Content() {
             <div className="d-flex justify-content-around w-75">
                 <span className="d-flex flex-column align-items-center justify-content-center rounded-border"> <p className="fs-4">{seconds}</p> <p>seconds</p></span>
                 <span className="d-flex flex-column align-items-center justify-content-center rounded-border"> <p className="fs-4">{score}</p> <p>score</p></span>
-                <span className="d-flex flex-column align-items-center justify-content-center rounded-border"> <p className="fs-4">{accuracy} %</p> <p>accuracy</p></span>
             </div>
             <input className="form-control w-50 mt-4" placeholder="Start typing here..." onKeyUp={handleEnteredWords} />
+            <ModalComponent isModalOpen={isModalOpen} score={score} index={index} setModalClose={setModalClose} />
         </div>
     )
 }
