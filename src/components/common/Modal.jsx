a7283b22d766914f903bfc32c1cb4aaab334707f
Kartik <kartik.ipatil10@gmail.com>
2022-12-17 22:18:51 +0530

    Feat: Added countdown

---
+++ src/components/common/Modal.jsx
@@ -0,0 +1,32 @@
+import { Modal, Typography, Box, Button } from "@mui/material";
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+export default function ModalComponent(props) {
+    const { isModalOpen, score, index, setModalClose } = props;
+    return (
+        <Modal
+            open={isModalOpen}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Box sx={style} className="d-flex flex-column align-items-center justify-content-center">
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Score: {score} <br />
+                    Accuracy: {((score / index) * 100).toFixed(2)}%
+                </Typography>
+                <Button className="mt-4" onClick={setModalClose} variant="outlined">Close</Button>
+            </Box>
+        </Modal>
+    )
+}
