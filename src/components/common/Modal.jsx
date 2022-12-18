868c5915fbce39bb632f99b042148a4dbc978c1f
Kartik <kartik.ipatil10@gmail.com>
2022-12-18 17:28:30 +0530

    Fix: UI fixes

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -23,8 +23,8 @@ export default function ModalComponent(props) {
         >
             <Box sx={style} className="d-flex flex-column align-items-center justify-content-center">
                 <Typography id="modal-modal-title" variant="h6" component="h2">
-                    Score: {score} <br />
-                    Accuracy: {((score / index) * 100).toFixed(2)}%
+                    Score: <b>{score}</b> <small>words/min</small> <br />
+                    Accuracy: <b>{((score / index) * 100).toFixed(2)}</b> <small>%</small>
                 </Typography>
                 <Button className="mt-4" onClick={setModalClose} variant="outlined">Close</Button>
             </Box>
